d04e8e33391bbe8bd432a76e5a67cbec832dd529
Rob Aird <r.aird@mumms.com>
2019-01-09 15:24:32 -0600

    added react-boilerplate 'rewrote generate templates for linting script' pr

--- internals/generators/language/index.js
+++ internals/generators/language/index.js
@@ -34,8 +34,24 @@ module.exports = {
     },
   ],
 
-  actions: () => {
+  actions: ({ test }) => {
     const actions = [];
+
+    if (test) {
+      // backup files that will be modified so we can restore them
+      actions.push({
+        type: 'backup',
+        path: '../../app',
+        file: 'i18n.js',
+      });
+
+      actions.push({
+        type: 'backup',
+        path: '../../app',
+        file: 'app.js',
+      });
+    }
+
     actions.push({
       type: 'modify',
       path: '../../app/i18n.js',
@@ -86,6 +102,17 @@ module.exports = {
       });
       return 'modify translation messages';
     });
+
+    if (!test) {
+      actions.push(() => {
+        const cmd = 'npm run extract-intl';
+        exec(cmd, (err, result) => {
+          if (err) throw err;
+          process.stdout.write(result);
+        });
+        return 'modify translation messages';
+      });
+    }
 
     return actions;
   },
